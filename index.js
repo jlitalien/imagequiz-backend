730e8013060629c52e49c6a3876749b1c5318be9
jlitalien <jlitalien@email.arizona.edu>
2022-03-31 21:54:25 -0700

    ok

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const flowers = require("./flowers");
 const quizzes = require("./data");
-const scores = require("./scores");
+let { customers } = require("./customers");
+let { scores } = require("./scores");
 const express = require("express");
+const e = require("express");
 const app = express();
 const port = 4002;
 
+app.use(express.json());
+
 app.get("/flowers", function (req, res) {
-  res.status(200).json({ done: true, result: flowers, message: "message" });
+  res.status(200).json({
+    done: true,
+    result: flowers,
+    message: "List of flowers returned!",
+  });
 });
 
 app.get("/quiz/:id", function (req, res) {
@@ -17,7 +25,11 @@ app.get("/quiz/:id", function (req, res) {
       resultArray = quizzes.quizzes[i];
     }
   }
-  res.status(200).json({ done: true, result: resultArray, message: "message" });
+  res.status(200).json({
+    done: true,
+    result: resultArray,
+    message: "Specified quiz returned!",
+  });
 });
 
 app.get("/scores/:quiztaker/:quizid", function (req, res) {
@@ -30,7 +42,97 @@ app.get("/scores/:quiztaker/:quizid", function (req, res) {
       resultNum = scores.scores[i].score;
     }
   }
-  res.status(200).json({ done: true, result: resultNum, message: "message" });
+  res.status(200).json({
+    done: true,
+    result: resultNum,
+    message: "Specified quiz scores returned!",
+  });
+});
+
+app.post("/register", (req, res) => {
+  var name = req.body.name;
+  var email = req.body.email;
+  var pwd = req.body.password;
+  var newID = Math.floor(Math.random(10000));
+  var newEntry = { id: newID, name: name, email: email, password: pwd };
+  if (customers.length === 0) {
+    customers.push(newEntry);
+    res
+      .status(200)
+      .json({ done: true, message: "User successfully added!" })
+      .end();
+  } else {
+    for (var i = 0; i < customers.length; i++) {
+      if (customers[i]["email"] === email) {
+        res
+          .status(403)
+          .json({
+            done: false,
+            message: "Entry with same email already exists",
+          })
+          .end();
+      }
+    }
+    customers.push(newEntry);
+    res
+      .status(200)
+      .json({ done: true, message: "User successfully added!" })
+      .end();
+  }
+});
+
+app.post("/login", (req, res) => {
+  var email = req.body.email;
+  var pwd = req.body.password;
+  if (customers.length === 0) {
+    res
+      .status(403)
+      .json({ done: false, message: "Invalid login credentials" })
+      .end();
+  } else {
+    for (var i = 0; i < customers.length; i++) {
+      if (customers[i]["email"] === email && customers[i]["password"] === pwd) {
+        res
+          .status(200)
+          .json({
+            done: true,
+            message: "Valid login credentials",
+          })
+          .end();
+      }
+    }
+    res
+      .status(403)
+      .json({ done: false, message: "Invalid login credentials" })
+      .end();
+  }
+});
+
+app.post("/score", (req, res) => {
+  var email = req.body.quizTaker;
+  var id = req.body.quizId;
+  var score = req.body.score;
+  var date = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
+
+  var newEntry = { quizTaker: email, quidId: id, score: score, date: date };
+  if (scores.length === 0) {
+    scores.push(newEntry);
+    res.status(200).json({ done: true, message: "Score updated" }).end();
+  } else {
+    for (var i = 0; i < scores.length; i++) {
+      if (scores[i]["quizTaker"] === email) {
+        res
+          .status(403)
+          .json({
+            done: false,
+            message: "Entry with same email already exists",
+          })
+          .end();
+      }
+    }
+    scores.push(newEntry);
+    res.status(200).json({ done: true, message: "Score updated" }).end();
+  }
 });
 
 app.listen(port, () => console.log("App listening on port 4002"));
